751075394011d5ab6a206e1b96c3acef62a4163c
Christopher Serrano <serrano.cjm@gmail.com>
2025-03-08 09:35:23 -0400

    state hoisting

--- app/src/main/java/site/cjserrano/basiccodelabs/MainActivity.kt
+++ app/src/main/java/site/cjserrano/basiccodelabs/MainActivity.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,12 +31,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val names: List<String> = listOf("World", "Compose")
         setContent {
             BasicCodelabsTheme {
                 MyApp(
-                    modifier = Modifier.fillMaxSize(),
-                    names
+                    modifier = Modifier.fillMaxSize()
                 ) // this is what sets the whole activity modifier
             }
         }
@@ -40,19 +43,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(
-    modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")
+    modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    // In Compose, UI elements are not hidden, but rather they aren't added to the composition
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
         }
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    // we add the preview for MyApp since the state will be hoisted
+    BasicCodelabsTheme {
+        MyApp(Modifier.fillMaxSize())
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember { mutableStateOf(false) } // This will remember the state value for each composable
-    val extraPadding = if (expanded.value) 48.dp else 0.dp // we add an extra padding to the composable when the button is clicked
+    val expanded =
+        remember { mutableStateOf(false) } // This will remember the state value for each composable
+    val extraPadding =
+        if (expanded.value) 48.dp else 0.dp // we add an extra padding to the composable when the button is clicked
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
@@ -69,7 +87,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             }
             ElevatedButton(
                 onClick = {
-                    expanded.value = !expanded.value // here we change the state value of the composable, which will be recomposed and then redrawn in the UI
+                    expanded.value =
+                        !expanded.value // here we change the state value of the composable, which will be recomposed and then redrawn in the UI
                 }
             ) {
                 Text(if (expanded.value) "Show Less" else "Show More")
@@ -84,5 +103,58 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BasicCodelabsTheme {
         MyApp()
+    }
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    // refactored to encapsulate
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingsPreview() {
+    BasicCodelabsTheme {
+        Greetings()
+    }
+}
+
+
+// Onboarding screen
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicCodelabsTheme {
+        // Since we have to pass a lambda to the onboarding screen for the button listener...
+        // we pass an empty statement
+        OnboardingScreen(onContinueClicked = {})
     }
 }
