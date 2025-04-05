594b785d7d32930fadd185ea72adf095c85db6c8
Christopher Serrano <serrano.cjm@gmail.com>
2025-04-05 10:26:49 -0400

    theming

--- app/src/main/java/site/cjserrano/basiccodelabs/MainActivity.kt
+++ app/src/main/java/site/cjserrano/basiccodelabs/MainActivity.kt
@@ -1,5 +1,6 @@
 package site.cjserrano.basiccodelabs
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,24 +12,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
@@ -99,7 +98,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp)) // here we add the modifier for the composable (to be executed after clicking the button)
             ) {
                 Text(text = "Hello ")
-                Text(text = "${name}!")
+                // you can add styles derived from Theme.kt... BasicCodelabsTheme inherits from MaterialTheme
+                //
+                Text(
+                    text = "${name}!",
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = {
@@ -114,11 +120,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 // the preview is for showing in the layout inspector?
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     BasicCodelabsTheme {
-        MyApp()
+        Greetings()
     }
 }
 
